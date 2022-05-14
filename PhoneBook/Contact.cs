bd148f00d004ea68782565ad1be00f986c1bea64
Michnikowski <michal.gwiazdonik@gmail.com>
2022-05-14 12:16:49 +0200

    Initial commit

---
+++ PhoneBook/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace PhoneBook
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; } 
+
+        public Contact(string firstName, string lastName, int phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+        }
+
+        
+    }
+}
