fdc0d01eae34b04b0622be8545f1c9e0113828bb
michnikowski <michal.gwiazdonik@gmail.com>
2022-05-29 19:52:38 +0200

    Add tests

---
+++ PhoneBook.Tests/ContactTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PhoneBook.Tests
+{
+    public class ContactTests
+    {
+        [Fact]
+
+        public void Contact_ForNewInstance_ReturnContactInstance()
+        {
+            Contact contact = new Contact("Mike", "Case", 123123123);
+
+            Assert.Equal("Mike", contact.FirstName);
+            Assert.Equal( "Case", contact.LastName);
+            Assert.Equal(123123123, contact.PhoneNumber);
+        }
+    }
+}
---
+++ PhoneBook.Tests/PhoneBookTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PhoneBook.Tests
+{
+    public class PhoneBookTests
+    {
+        PhoneBook phoneBook = new PhoneBook();
+        
+        [Fact]        
+
+        public void AddContact_AppendContactToPhoneBook_ReturnPhoneBookContactsCountBeforeAndAfter()
+        {
+            int phoneBookCountBefore = phoneBook.Contacts.Count;
+
+            phoneBook.AddContact("Adam", "Boll", 123123123);
+
+            Assert.Equal(phoneBookCountBefore + 1, phoneBook.Contacts.Count);
+        }
+    }
+}
